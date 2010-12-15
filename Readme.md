a04c4fd7b462262407dcbb2d672ba1778fad9948
Jim Morris <morris@wolfman.com>
2010-12-14 17:39:29 -0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+Anything On Rails
+==========
+
+I like the various rails plugins on emacs, but for what I want they
+are too heavy.
+
+I use [Anything](http://www.emacswiki.org/emacs/Anything) in Emacs a
+lot, and for things like quick navigation I think it is all I need.
+
+So here is a very lightweight Anything Source that shows all the major
+files in a rails project, so navigation is pretty easy.
+
+I also added [RubyKitchs](http://www.emacswiki.org/emacs/rubikitch)
+anything Rake source so you can run rake tasks pretty easily too.
+
+I'll be adding stuff to this as needed, but I want to keep it small,
+and not compete with Rinari or rails-mode-reloaded.
---
+++ anything-rails.el
@@ -0,0 +1,159 @@
+;;; anything-rails.el --- 
+
+;; Copyright (C) 2010 Jim Morris
+
+;; Author: Jim Morris <morris@wolfman.com>
+;; Version: 0.1
+;; URL: http://blog.wolfman.com
+
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+;;
+;;; depends on
+;; find-cmd
+;; anything
+;; anything-config
+;;
+;;; Commentary:
+;;
+;; An anything source that shows major files in a rails project
+;;
+
+
+(require 'find-cmd)
+
+(eval-when-compile
+  (defvar anything-project-root))
+
+;;
+;; Handle anything integration with rails project view
+;;
+
+(defvar rails-directories
+  (list "app" "spec" "config" "lib")
+  "List of directories in rails root to search for files")
+
+(defun rails-make-displayable-name (path)
+  "makes path into a displayable name. eg view(post): file, model: file, controller: name"
+  (let ((dir (file-name-directory path))
+		(name (file-name-nondirectory path)))
+	(let
+		((type (cond 
+			   ((string-match "/app/views/\\([a-zA-Z0-9_]+\\)/" dir)
+				(concat "view(" (match-string 1 dir) ")"))
+			   ((string-match "/app/controllers/.*/\\([a-zA-Z0-9_]+\\)Controller" dir) "controller")
+			   ((string-match "/app/models/" dir) "model")
+			   ((string-match "/config/\\([a-zA-Z0-9_]+\\)/" dir)
+				(concat "config(" (match-string 1 dir) ")"))
+			   ((string-match "/config/" dir) "config(root)")
+			   ((string-match "/spec/\\([a-zA-Z0-9_]+\\)/" dir)
+				(concat "spec(" (match-string 1 dir) ")"))
+			   ((string-match "/spec/" dir) "spec(root)")
+			   ((string-match "/app/helpers/" dir) "helper")
+			   ((string-match "/app/mailers/" dir) "mailer")
+			   ((string-match "/app/\\([a-zA-Z0-9_]+\\)/" dir) (match-string 1 dir))
+			   (t "misc file"))))
+	
+	  (concat type ": " name))))
+
+(defun rails-dirs (root)
+  "Returns list of directories to search in rails" 
+  (mapconcat 'identity 
+			 (mapcar (lambda (x) (concat root x)) rails-directories )
+			 " "))
+  
+
+(defun rails-list-project-files ()
+  "Returns a list of all files found under the rails project."
+
+  ;; find root of project return empty list if not a rails project
+  ;; checks for Gemfile in the RAILS_ROOT and for config/application.rb (Rails3)
+  (let ((rails-project-root (locate-dominating-file default-directory "Gemfile")))
+	(when (and rails-project-root
+			   (file-exists-p (concat rails-project-root "config/application.rb")))
+	  ;; get a list of all the relevant files
+	  (let ((rails-project-files-list (split-string 
+									   (shell-command-to-string 
+										(concat "find " (rails-dirs rails-project-root) 
+												" "
+												(find-to-string 
+												 '(prune (name ".svn" ".git"))) 
+												" "
+												(find-to-string 
+												 `(or (name "*.rb" "*.haml" "*.erb" "*.yml"))))))))
+
+		;; convert the list into cons pair of (display . filepath) where
+		;; display is a friendly name
+		(mapcar
+		 (lambda (f)
+		   (cons (rails-make-displayable-name f) f)) rails-project-files-list)))))
+
+
+;; anything source for showing all rails project files
+(defvar anything-c-source-rails-project-files
+  '((name . "Files in Rails Project")
+	(candidates . (lambda () (rails-list-project-files)))
+	(match anything-c-match-on-file-name)
+	;(candidate-transformer nil)
+    (type . file)))
+
+(defvar rails-anything-related-files
+  '((name . "Related Files in Rails Project")
+	(candidates . (lambda () (rails-list-project-files)))
+	(match anything-c-match-on-file-name)
+	;(candidate-transformer nil)
+    (type . file)))
+
+;; Shows any related files to the current file in anywhere
+(defun rails-show-related-files ()
+  "Uses Anything to show related files"
+  (interactive)
+  (anything '(rails-anything-related-files)))
+
+;;
+;; Rake tasks by http://www.emacswiki.org/emacs/rubikitch
+;;
+
+(defvar anything-current-buffer nil)
+(defadvice anything (before get-current-buffer activate)
+  (setq anything-current-buffer (current-buffer)))
+
+(setq anything-c-source-rake-task
+  '((name . "Rake Task")
+    (candidates
+     . (lambda ()
+         (when (string-match "^rake" anything-pattern)
+           (cons '("rake" . "rake")
+                 (mapcar (lambda (line)
+                           (cons line (car (split-string line " +#"))))
+                         (with-current-buffer anything-current-buffer
+                           (split-string (shell-command-to-string "rake -T") "\n" t)))))))
+    (action ("Compile" . compile)
+            ("Compile with command-line edit"
+             . (lambda (c) (let ((compile-command (concat c " ")))
+                             (call-interactively 'compile)))))
+    (requires-pattern . 4)))
+
+(defun rake ()
+  "Uses Anything to show and execute rake tasks"
+  (interactive)
+  (if (locate-dominating-file default-directory "Rakefile")
+	  (anything '(anything-c-source-rake-task) "rake")
+	(message "No Rakefile available")))
+  
+
+(provide 'anything-rails)
